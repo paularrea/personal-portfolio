5302194a57d8d2dfd0d147bea460e1d599817120
Pau Larrea <paularrea12@hotmail.com>
2020-09-17 17:39:47 +0200

    burger

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import headerStyles from "./header.module.scss"
 import Uv from "../UvToggle/uv"
+import Burger from "./burger"
 
 const Header = () => {
   const data = useStaticQuery(graphql`
@@ -15,56 +16,62 @@ const Header = () => {
   `)
 
   return (
-    <header className={headerStyles.header}>
-      <nav>
-        <ul
-          className={headerStyles.navList}
-          style={{
-            backgroundColor: "var(--bg)",
-          }}
-        >
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/"
+    <div>
+      <header className={headerStyles.header}>
+        {window.innerWidth >= 500 ? (
+          <nav>
+            <ul
+              className={headerStyles.navList}
+              style={{
+                backgroundColor: "var(--bg)",
+              }}
             >
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/projects"
-            >
-              PROJECTS
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/about"
-            >
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
-              to="/contact"
-            >
-              CONTACT
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={headerStyles.uv}>
-        <Uv />
-      </div>
-    </header>
+              <li>
+                <Link
+                  className={headerStyles.navItem}
+                  activeClassName={headerStyles.activeNavItem}
+                  to="/"
+                >
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={headerStyles.navItem}
+                  activeClassName={headerStyles.activeNavItem}
+                  to="/projects"
+                >
+                  PROJECTS
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={headerStyles.navItem}
+                  activeClassName={headerStyles.activeNavItem}
+                  to="/about"
+                >
+                  ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={headerStyles.navItem}
+                  activeClassName={headerStyles.activeNavItem}
+                  to="/contact"
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <Burger />
+        )}
+        <div className={headerStyles.uv}>
+          <Uv />
+        </div>
+      </header>
+    </div>
   )
 }
 
