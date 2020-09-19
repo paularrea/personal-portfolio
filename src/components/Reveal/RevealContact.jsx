8addbe91b35dda3981764746ec822a46743573ef
Pau Larrea <paularrea12@hotmail.com>
2020-09-19 15:23:57 +0200

    btns

--- src/components/Reveal/RevealContact.jsx
+++ src/components/Reveal/RevealContact.jsx
@@ -7,26 +7,32 @@ const RevealContact = () => {
   return (
     <Fade left delay={300} duration={2000}>
       <div className={style.container}>
-          <h3 className={style.letscreate}>
-            Let's <span className={style.create}>create</span>.
-          </h3>
-          <p className={style.reach_me}>Reach me out!</p>
-          <div className={style.links_container}>
-            <div className={style.btn}>
-              <a href={cv} target="_blank" rel="noopener noreferrer">
-                Download CV
-              </a>
-            </div>
-            <div className={style.btn}>
-              <a
-                href="mailto:paularrea12@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Get in touch
-              </a>
-            </div>
+        <h3 className={style.letscreate}>
+          Let's <span className={style.create}>create</span>.
+        </h3>
+        <p className={style.reach_me}>Reach me out!</p>
+        <div className={style.links_container}>
+          <div>
+            <a
+              className={style.btn}
+              href={cv}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download CV
+            </a>
           </div>
+          <div>
+            <a
+              className={style.btn}
+              href="mailto:paularrea12@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Get in touch
+            </a>
+          </div>
+        </div>
       </div>
     </Fade>
   )
