aa6fb4b62c713f9078531477a0cb247ad81b97f0
Pau Larrea <paularrea12@hotmail.com>
2020-09-20 21:40:35 +0200

    d

--- src/components/Video/introVideo.jsx
+++ src/components/Video/introVideo.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import videoStyle from "./introVideo.module.scss"
 import ReactPlayer from "react-player/lazy"
+import url from "../../../static/JOSIE.mp4"
 
 function IntroVideo() {
   return (
     <ReactPlayer
-      url="https://firebasestorage.googleapis.com/v0/b/innovision-248ab.appspot.com/o/team%2FJOSIE.mp4?alt=media&token=037636e5-2107-4553-bcfc-8bb6420a0835"
+      url={url}
       playing
       muted
       loop
