fb101f6ad8ca9ef75197f7caaf48c370ddb8f50d
Pau Larrea <paularrea12@hotmail.com>
2020-09-21 16:56:11 +0200

    ok

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,10 +2,10 @@ import React from "react"
 import emailjs from "emailjs-com"
 import styles from "../styles/contact.module.scss"
 import Layout from "../components/Layout/layout"
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+// import Swal from 'sweetalert2'
+// import withReactContent from 'sweetalert2-react-content'
 
-const MySwal = withReactContent(Swal)
+// const MySwal = withReactContent(Swal)
 
 export default function Contact() {
   function sendEmail(e) {
@@ -20,19 +20,21 @@ export default function Contact() {
       )
       .then(
         result => {
-          MySwal.fire({
-            icon: 'success',
-            title: 'Your message has been sent',
-            text: 'I will answer you as soon as possible!',
-          })
+          // MySwal.fire({
+          //   icon: 'success',
+          //   title: 'Your message has been sent',
+          //   text: 'I will answer you as soon as possible!',
+          // })
+          alert('Your message has been sent')
           console.log(result.text)
         },
         error => {
-          MySwal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!',
-          })
+          // MySwal.fire({
+          //   icon: 'error',
+          //   title: 'Oops...',
+          //   text: 'Something went wrong!',
+          // })
+          alert('Something went wrong...')
           console.log(error.text)
         }
       )
