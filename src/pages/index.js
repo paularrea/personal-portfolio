7d3c63552b26eda52546eebc9cbb7f781c6e11e9
Pau Larrea <paularrea12@hotmail.com>
2020-10-27 17:36:56 +0100

    intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,41 @@
-// import React, { useState, useEffect } from "react"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "../components/Layout/layout"
 import Home from "../components/web-pages/home"
 import About from "../components/web-pages/about"
 import Contact from "../components/web-pages/contact"
 import Projects from "../components/web-pages/projects"
-// import style from "../styles/initial.animation.module.scss"
-// import Fade from "react-reveal/Fade"
+import style from "../styles/home.module.scss"
+import Fade from "react-reveal/Fade"
 
 const App = () => {
-  // const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false)
 
-  // useEffect(() => {
-  //   const intro = setTimeout(() => {
-  //     setLoaded(true)
-  //   }, 2000)
-  //   return () => clearTimeout(intro)
-  // }, [])
+  useEffect(() => {
+    const intro = setTimeout(() => {
+      setLoaded(true)
+    }, 2000)
+    return () => clearTimeout(intro)
+  }, [])
 
   return (
-    // <div>
-    //   {loaded ? (
-    //     <Layout>
-    //       <Home />
-    //       <Projects />
-    //       <About />
-    //       <Contact />
-    //     </Layout>
-    //   ) : (
-    //     <div className={style.on_load}>
-    //       <Fade top>
-    //       <div className={style.animation}>Pau Larrea.</div>
-    //       </Fade>
-    //     </div>
-    //   )}
-    // </div>
     <div>
-      <Layout>
-        <Home />
-        <Projects />
-        <About />
-        <Contact />
-      </Layout>
+      {loaded ? (
+        <Layout>
+          <Home />
+          <Projects />
+          <About />
+          <Contact />
+        </Layout>
+      ) : (
+        <div className={style.on_load}>
+          <Fade top>
+          <div className={style.animation}>Pau Larrea.</div>
+          </Fade>
+        </div>
+      )}
     </div>
+
   )
 }
 
