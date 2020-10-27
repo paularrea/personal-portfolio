dc6da5f1569fed7ab29bc7a83283ad8b4714211c
Pau Larrea <paularrea12@hotmail.com>
2020-10-27 17:18:54 +0100

    clean

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,50 @@
+// import React, { useState, useEffect } from "react"
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "../components/Layout/layout"
 import Home from "../components/web-pages/home"
 import About from "../components/web-pages/about"
 import Contact from "../components/web-pages/contact"
 import Projects from "../components/web-pages/projects"
-// import Site from "./site"
+// import style from "../styles/initial.animation.module.scss"
+// import Fade from "react-reveal/Fade"
 
 const App = () => {
+  // const [loaded, setLoaded] = useState(false)
+
+  // useEffect(() => {
+  //   const intro = setTimeout(() => {
+  //     setLoaded(true)
+  //   }, 2000)
+  //   return () => clearTimeout(intro)
+  // }, [])
+
   return (
-    <Layout>
-      <Home />
-      <Projects />
-      <About />
-      <Contact />
-    </Layout>
+    // <div>
+    //   {loaded ? (
+    //     <Layout>
+    //       <Home />
+    //       <Projects />
+    //       <About />
+    //       <Contact />
+    //     </Layout>
+    //   ) : (
+    //     <div className={style.on_load}>
+    //       <Fade top>
+    //       <div className={style.animation}>Pau Larrea.</div>
+    //       </Fade>
+    //     </div>
+    //   )}
+    // </div>
+    <div>
+      <Layout>
+        <Home />
+        <Projects />
+        <About />
+        <Contact />
+      </Layout>
+    </div>
   )
 }
+
 export default App
