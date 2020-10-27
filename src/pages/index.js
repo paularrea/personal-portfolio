d00480c7a350032f7b027ee4d69462e11c45a0f4
Pau Larrea <paularrea12@hotmail.com>
2020-10-27 16:59:01 +0100

    no_intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import Site from './site';
+import React from "react"
+import Layout from "../components/Layout/layout"
+import Home from "../components/web-pages/home"
+import About from "../components/web-pages/about"
+import Contact from "../components/web-pages/contact"
+import Projects from "../components/web-pages/projects"
+// import Site from "./site"
 
 const App = () => {
   return (
-    <Site/> 
+    <Layout>
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
+    </Layout>
   )
 }
-export default App;
+export default App
