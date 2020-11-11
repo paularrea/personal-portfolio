5926ee41aa1d48aff6dd563ba69381f89af1cc18
Pau Larrea <paularrea12@hotmail.com>
2020-11-11 16:38:16 +0100

    betterUI

--- src/components/infoHome/InfiniteSroll.jsx
+++ src/components/infoHome/InfiniteSroll.jsx
@@ -9,14 +9,14 @@ const InfiniteSroll = () => {
         width: '100vw',
         whiteSpace: 'nowrap',
         padding: '1rem .3rem',
-        borderTop: '1px solid var(--textNormal)',
-        borderBottom: '1px solid var(--textNormal)',
+        borderTop: '.12rem solid var(--textNormal)',
+        borderBottom: '.12rem solid var(--textNormal)',
       }}
     >
       <Marquee direction='left' speed={0.09}>
-    MY NAME IS PAU LARREA  -  VERY NICE TO MEET YOU!  -  PLEASE TAKE A LOOK
+    <b> VERY NICE TO MEET YOU!  -  PLEASE TAKE A LOOK
     AROUND  -  HERE YOU CAN FIND A BIT OF MYSELF AND SOME OF MY PERSONAL
-    PROJECTS  -  LET'S CREATE TOGETHER  - 
+    PROJECTS  -  LET'S CREATE TOGETHER  - </b>
       </Marquee>
     </div>
   )
